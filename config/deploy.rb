6981c1a41c7904fc7ac5bdc7de465517dfef0fb5
Evgeny Lapin <lev@openteam.ru>
2015-09-15 11:12:53 +0600

    fixed download files capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,14 @@
 require 'openteam/capistrano/recipes'
 
 namespace :images do
-  desc 'Upload specialities.csv to production'
-  task :upload do
-    transfer :up, "public/system", "#{shared_path}/system", :via =>:scp, :recursive =>:true
-  end
-
-  desc 'Download specialities.csv from production'
+  desc 'Download public/system, tmp/cache & tmp/index from production'
   task :download do
-    transfer :down, "#{shared_path}/system", "public/system", :via =>:scp, :recursive =>:true
+    transfer :down, "#{shared_path}/system",  'public', :via =>:scp, :recursive =>:true
+    transfer :down, "#{shared_path}/cache",   'tmp',    :via =>:scp, :recursive =>:true
+    transfer :down, "#{shared_path}/index",   'tmp',    :via =>:scp, :recursive =>:true
   end
 end
 
-set :shared_children, fetch(:shared_children) + %w[public/system]
+set :shared_children, fetch(:shared_children) + %w[public/system tmp/cache tmp/index]
 
 set :default_stage, 'production'
